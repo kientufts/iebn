695c3b2db058a4132a4e2f924bfb9dd65ea6e0ad
Iansky <letrungkien1991@gmail.com>
2024-03-27 14:58:00 -0700

    Initial commit

---
+++ iebn.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS iebn;
+CREATE DATABASE iebn;
+
+
+-- FICO --
+CREATE TABLE fico (
+  month INT PRIMARY KEY,
+  chase INT,
+  boa INT,
+  citi INT,
+  dcu INT,
+  discover INT,
+  transUnion INT,
+  equiFax INT
+)
